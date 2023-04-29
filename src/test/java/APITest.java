7bd7f90b76d6f319840a812151e234c4d83a5301
Neto.Changes <infero-sky@yandex.ru>
2023-04-29 23:11:27 +0300

    Failed test

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -16,7 +16,7 @@ public class APITest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some vata"))
         ;
     }
 
